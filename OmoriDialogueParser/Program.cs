de81f60107d607ac476d642477e39a9d809a1989
goat <16760685+goaaats@users.noreply.github.com>
2020-12-30 14:43:34 +0100

    feat: add version sorted by character

--- OmoriDialogueParser/Program.cs
+++ OmoriDialogueParser/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using OmoriDialogueParser.Model;
 using OmoriDialogueParser.Model.MessageTextPayloads;
@@ -75,6 +76,32 @@ namespace OmoriDialogueParser
             }
 
             File.WriteAllText(Path.Combine("html", "text.js"), $"var t = {JsonConvert.SerializeObject(fileParsedDict)}");
+
+            // Create another version of the object, sorted by character name
+            var byCharaDict = new Dictionary<string, Dictionary<string, List<ExportMessage>>>();
+
+            foreach (var parsed in fileParsedDict)
+            {
+                foreach (var message in parsed.Value)
+                {
+                    if (string.IsNullOrEmpty(message.Speaker))
+                        continue;
+
+                    // We want to filter all of the "time" messages
+                    if (Regex.IsMatch(message.Speaker, "[0-9].*"))
+                        continue;
+
+                    if (!byCharaDict.ContainsKey(message.Speaker))
+                        byCharaDict.Add(message.Speaker, new Dictionary<string, List<ExportMessage>>());
+
+                    if (!byCharaDict[message.Speaker].ContainsKey(parsed.Key))
+                        byCharaDict[message.Speaker].Add(parsed.Key, new List<ExportMessage>());
+
+                    byCharaDict[message.Speaker][parsed.Key].Add(message);
+                }
+            }
+
+            File.WriteAllText(Path.Combine("html", "text_bychara.js"), $"var t = {JsonConvert.SerializeObject(byCharaDict)}");
         }
     }
 }
